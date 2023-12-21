2d6020baa30b1967b6a55c7de80b8dd4b364984b
Vikas Jaiswal <vikasjaiswal.coder@gmail.com>
2023-12-21 09:53:56 +0530

    Modified Info Page and Added Go Back Animation

--- UnrealFurniture/app/src/main/java/com/vikasjaiswal/unrealfurniture/InfoActivity.kt
+++ UnrealFurniture/app/src/main/java/com/vikasjaiswal/unrealfurniture/InfoActivity.kt
@@ -4,15 +4,28 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.animation.AnimationUtils
+import com.google.android.material.imageview.ShapeableImageView
 
 class InfoActivity : AppCompatActivity() {
+
+    lateinit var goback : ShapeableImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.info_activity)
+
+        goback = findViewById(R.id.goback)
+
+        val gobackanim = AnimationUtils.loadAnimation(this, R.anim.gobackanim)
+        goback.startAnimation(gobackanim)
     }
 
     fun openSupport(view: View) {
         val intent =  Intent(this@InfoActivity, SupportActivity::class.java)
         startActivity(intent)
+    }
+
+    fun goBack(view: View) {
+        finish()
     }
 }
