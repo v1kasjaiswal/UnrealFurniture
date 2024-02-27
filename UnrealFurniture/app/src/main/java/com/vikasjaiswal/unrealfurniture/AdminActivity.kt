232751602f12e3bb7e1e1a80b769760f3005bdc0
Vikas Jaiswal <vikasjaiswal.coder@gmail.com>
2024-02-27 21:47:09 +0530

    Added AddProduct Fragment and Designed UI

--- UnrealFurniture/app/src/main/java/com/vikasjaiswal/unrealfurniture/AdminActivity.kt
+++ UnrealFurniture/app/src/main/java/com/vikasjaiswal/unrealfurniture/AdminActivity.kt
@@ -1,17 +1,23 @@
 package com.vikasjaiswal.unrealfurniture
 
 import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.cardview.widget.CardView
+import androidx.fragment.app.Fragment
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.material.navigation.NavigationBarView
 import com.google.firebase.auth.FirebaseAuth
 
 class AdminActivity : AppCompatActivity() {
+
+    private lateinit var networkReceiver: CheckConnectivity
+    private lateinit var bottomNav: NavigationBarView
 
     var auth = FirebaseAuth.getInstance()
     lateinit var googleSignInClient: GoogleSignInClient
@@ -32,6 +38,14 @@ class AdminActivity : AppCompatActivity() {
 
         googleSignInClient = GoogleSignIn.getClient(this, gsio)
 
+        networkReceiver = CheckConnectivity()
+        bottomNav = findViewById(R.id.adminbottomnav)
+
+        setupFragment(AnalyticsFragment())
+
+        bottomNav.setOnItemSelectedListener { handleNavigation(it.itemId) }
+        bottomNav.setOnItemReselectedListener { handleNavigation(it.itemId) }
+
 
         signOut.setOnClickListener {
             try {
@@ -47,4 +61,39 @@ class AdminActivity : AppCompatActivity() {
             }
         }
     }
+
+    private fun setupFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
+            .replace(R.id.fragment_container, fragment)
+            .commitNow()
+    }
+
+    private fun handleNavigation(itemId: Int): Boolean {
+        val fragment = when (itemId) {
+            R.id.analytics -> AnalyticsFragment()
+            R.id.addproduct -> AddProductFragment()
+            R.id.transactions -> TransactionsFragment()
+            else -> return false
+        }
+
+        setupFragment(fragment)
+        return true
+    }
+
+    override fun onResume() {
+        super.onResume()
+        registerConnectivityReceiver()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        unregisterReceiver(networkReceiver)
+    }
+
+    private fun registerConnectivityReceiver() {
+        val intentFilter = IntentFilter("android.net.conn.CONNECTIVITY_CHANGE")
+        registerReceiver(networkReceiver, intentFilter)
+    }
+
 }
