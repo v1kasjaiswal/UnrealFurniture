0eed1d54448ceb27dca7ae5f7016c2b7a59ab2a8
Vikas Jaiswal <vikasjaiswal.coder@gmail.com>
2024-03-14 19:42:38 +0530

    Added Functionality and Fixed Bug in Search Fragment

--- UnrealFurniture/app/src/main/java/com/vikasjaiswal/unrealfurniture/SearchFragment.kt
+++ UnrealFurniture/app/src/main/java/com/vikasjaiswal/unrealfurniture/SearchFragment.kt
@@ -5,9 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioGroup
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.chip.Chip
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class SearchFragment : Fragment() {
 
@@ -17,10 +26,17 @@ class SearchFragment : Fragment() {
 
     private lateinit var searchRecyclerView: RecyclerView
 
-    lateinit var sortBy : Chip
+    private lateinit var filterBottomSheetDialog: BottomSheetDialog
+    private lateinit var categoryBottomSheetDialog: BottomSheetDialog
+
+    lateinit var categoryRadioGroup: RadioGroup
+    lateinit var filterRadioGroup: RadioGroup
+
     lateinit var filterBy : Chip
     lateinit var selectCategory: Chip
     lateinit var outOfStock : Chip
+
+    lateinit var searchView : SearchView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -39,14 +55,27 @@ class SearchFragment : Fragment() {
 
         searchRecyclerView.adapter = searchAdapter
 
-        sortBy = view.findViewById(R.id.sortBy)
         filterBy = view.findViewById(R.id.filterBy)
         selectCategory = view.findViewById(R.id.selectCategory)
         outOfStock = view.findViewById(R.id.outOfStock)
 
-        sortBy.setOnClickListener {
-            openSortByBottomSheet()
-        }
+        searchView = view.findViewById(R.id.searchView)
+
+        filterBottomSheetDialog = BottomSheetDialog(requireContext())
+        val filterBottomSheetView = layoutInflater.inflate(R.layout.sortby_bottomresource, null)
+        filterBottomSheetDialog.setContentView(filterBottomSheetView)
+
+        filterRadioGroup = filterBottomSheetDialog.findViewById<RadioGroup>(R.id.filterRadioGroup)!!
+        filterRadioGroup.check(R.id.priceLowToHigh)
+
+        categoryBottomSheetDialog = BottomSheetDialog(requireContext())
+        val categoryBottomSheetView = layoutInflater.inflate(R.layout.categories_bottomresource, null)
+        categoryBottomSheetDialog.setContentView(categoryBottomSheetView)
+
+        categoryRadioGroup = categoryBottomSheetDialog.findViewById<RadioGroup>(R.id.categoryRadioGroup)!!
+        categoryRadioGroup.check(R.id.allCategories)
+
+        outOfStock.isChecked = false
 
         filterBy.setOnClickListener {
             openFilterByBottomSheet()
@@ -56,29 +85,159 @@ class SearchFragment : Fragment() {
             openSelectCategoryBottomSheet()
         }
 
-        outOfStock.setOnClickListener {
-            includeOutOfStockProducts()
-        }
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                searchAdapter?.updateData(query!!)
+                return true
+            }
 
-
-
+            override fun onQueryTextChange(newText: String?): Boolean {
+                searchAdapter?.updateData(newText!!)
+                return true
+            }
+        })
 
         return view
     }
 
-    private fun openSortByBottomSheet(){
-        
-    }
-
     private fun openFilterByBottomSheet(){
+        filterBottomSheetDialog.show()
 
+        filterRadioGroup.setOnCheckedChangeListener { group, checkedId ->
+            val category = when(categoryRadioGroup.checkedRadioButtonId){
+                R.id.filterSofas -> "Sofas"
+                R.id.filterBeds -> "Beds"
+                R.id.filterTables -> "Tables"
+                R.id.filterChairs -> "Chairs"
+                R.id.filterBookshelves -> "Bookshelves"
+                R.id.filterWardrobes -> "Wardrobes"
+                R.id.filterOthers -> "Others"
+                else -> "All"
+            }
+
+            when(checkedId){
+                R.id.priceLowToHigh -> {
+                    filterBottomSheetDialog.dismiss()
+
+                    if (categoryRadioGroup.checkedRadioButtonId == R.id.allCategories){
+                        searchAdapter?.updateData("PriceLowToHigh", outOfStock.isChecked)
+                    }
+                    else{
+                        searchAdapter?.updateData("PriceLowToHigh", category, outOfStock.isChecked)
+                    }
+                }
+                R.id.priceHighToLow -> {
+                    filterBottomSheetDialog.dismiss()
+
+                    if (categoryRadioGroup.checkedRadioButtonId == R.id.allCategories){
+                        searchAdapter?.updateData("PriceHighToLow", outOfStock.isChecked)
+                    }
+                    else{
+                        searchAdapter?.updateData("PriceHighToLow", category, outOfStock.isChecked)
+                    }
+                }
+                R.id.ratingLowToHigh -> {
+                    filterBottomSheetDialog.dismiss()
+
+                    if (categoryRadioGroup.checkedRadioButtonId == R.id.allCategories){
+                        searchAdapter?.updateData("RatingLowToHigh", outOfStock.isChecked)
+                    }
+                    else{
+                        searchAdapter?.updateData("RatingLowToHigh", category, outOfStock.isChecked)
+                    }
+                }
+                R.id.ratingHighToLow -> {
+                    filterBottomSheetDialog.dismiss()
+
+                    if (categoryRadioGroup.checkedRadioButtonId == R.id.allCategories){
+                        searchAdapter?.updateData("RatingHighToLow", outOfStock.isChecked)
+                    }
+                    else{
+                        searchAdapter?.updateData("RatingHighToLow", category, outOfStock.isChecked)
+                    }
+                }
+                R.id.discountLowToHigh -> {
+                    filterBottomSheetDialog.dismiss()
+
+                    if (categoryRadioGroup.checkedRadioButtonId == R.id.allCategories){
+                        searchAdapter?.updateData("DiscountLowToHigh", outOfStock.isChecked)
+                    }
+                    else{
+                        searchAdapter?.updateData("DiscountLowToHigh", category, outOfStock.isChecked)
+                    }
+                }
+                R.id.discountHighToLow -> {
+                    filterBottomSheetDialog.dismiss()
+
+                    if (categoryRadioGroup.checkedRadioButtonId == R.id.allCategories){
+                        searchAdapter?.updateData("DiscountHighToLow", outOfStock.isChecked)
+                    }
+                    else{
+                        searchAdapter?.updateData("DiscountHighToLow", category, outOfStock.isChecked)
+                    }
+                }
+            }
+        }
     }
 
     private fun openSelectCategoryBottomSheet(){
+        categoryBottomSheetDialog.show()
 
+        categoryRadioGroup.setOnCheckedChangeListener { group, checkedId ->
+
+            val sortBy = when(filterRadioGroup.checkedRadioButtonId){
+                R.id.priceLowToHigh -> "PriceLowToHigh"
+                R.id.priceHighToLow -> "PriceHighToLow"
+                R.id.ratingLowToHigh -> "RatingLowToHigh"
+                R.id.ratingHighToLow -> "RatingHighToLow"
+                R.id.discountLowToHigh -> "DiscountLowToHigh"
+                R.id.discountHighToLow -> "DiscountHighToLow"
+                else -> "PriceLowToHigh"
+            }
+
+            when(checkedId){
+                R.id.allCategories -> {
+                    categoryBottomSheetDialog.dismiss()
+
+                    searchAdapter?.updateData(sortBy, outOfStock.isChecked)
+                }
+                R.id.filterSofas -> {
+                    categoryBottomSheetDialog.dismiss()
+
+                    searchAdapter?.updateData(sortBy, "Sofas", outOfStock.isChecked)
+                }
+                R.id.filterBeds -> {
+                    categoryBottomSheetDialog.dismiss()
+
+                    searchAdapter?.updateData(sortBy, "Beds", outOfStock.isChecked)
+                }
+                R.id.filterTables -> {
+                    categoryBottomSheetDialog.dismiss()
+
+                    searchAdapter?.updateData(sortBy, "Tables", outOfStock.isChecked)
+                }
+                R.id.filterChairs -> {
+                    categoryBottomSheetDialog.dismiss()
+
+                    searchAdapter?.updateData(sortBy, "Chairs", outOfStock.isChecked)
+                }
+                R.id.filterBookshelves -> {
+                    categoryBottomSheetDialog.dismiss()
+
+                    searchAdapter?.updateData(sortBy, "Bookshelves", outOfStock.isChecked)
+                }
+                R.id.filterWardrobes -> {
+                    categoryBottomSheetDialog.dismiss()
+
+                    searchAdapter?.updateData(sortBy, "Wardrobes", outOfStock.isChecked)
+                }
+                R.id.filterOthers -> {
+                    categoryBottomSheetDialog.dismiss()
+
+                    searchAdapter?.updateData(sortBy, "Others", outOfStock.isChecked)
+                }
+            }
+        }
     }
 
-    private fun includeOutOfStockProducts(){
-
-    }
 }
--- UnrealFurniture/app/src/main/java/com/vikasjaiswal/unrealfurniture/SearchRecAdapter.kt
+++ UnrealFurniture/app/src/main/java/com/vikasjaiswal/unrealfurniture/SearchRecAdapter.kt
@@ -1,16 +1,74 @@
 package com.vikasjaiswal.unrealfurniture
 
+import android.content.Intent
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.RatingBar
+import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import com.faltenreich.skeletonlayout.Skeleton
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.squareup.picasso.Picasso
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 
 class SearchRecAdapter : RecyclerView.Adapter<SearchRecAdapter.ViewHolder>() {
 
-    private var itemCountLimit = 4
+    var productIds = listOf<String>()
+    var productMainImages = listOf<String>()
+    var productNames = listOf<String>()
+    var productRealPrices = listOf<String>()
+    var productDiscounts = listOf<String>()
+    var productDiscountedPrices = listOf<String>()
+    var productRatings = listOf<String>()
+    var productRatingCounts = listOf<String>()
+    var productStock = listOf<String>()
+
+    var db = FirebaseFirestore.getInstance()
+    var auth = FirebaseAuth.getInstance()
+
+    init {
+        updateData()
+    }
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        lateinit var selectedProductCard : CardView
 
+        lateinit var prodMainImage: ImageView
+        lateinit var prodName: TextView
+        lateinit var realPrice: TextView
+        lateinit var discount: TextView
+        lateinit var discountedPrice: TextView
+        lateinit var rating: RatingBar
+        lateinit var ratingCount: TextView
+
+        lateinit var skeleton: Skeleton
+        init {
+            selectedProductCard = itemView.findViewById(R.id.checkoutProdCard)
+
+            prodMainImage = itemView.findViewById(R.id.checkoutProdMainImage)
+            prodName = itemView.findViewById(R.id.checkoutProdName)
+            realPrice = itemView.findViewById(R.id.checkoutProdRealPrice)
+            discount = itemView.findViewById(R.id.checkoutProdQuantity)
+            discountedPrice = itemView.findViewById(R.id.checkoutProdDiscountedPrice)
+            rating = itemView.findViewById(R.id.checkoutProdRatingBar)
+            ratingCount = itemView.findViewById(R.id.checkoutProdRatingCounts)
+
+            skeleton = itemView.findViewById(R.id.skeletonLayout)
+
+            realPrice.paint.isStrikeThruText = true
+
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -21,10 +79,213 @@ class SearchRecAdapter : RecyclerView.Adapter<SearchRecAdapter.ViewHolder>() {
     }
 
     override fun getItemCount(): Int {
-        return itemCountLimit
+        return productIds.size
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        // Load data for each item here if needed
+        holder.skeleton.showSkeleton()
+        Handler(Looper.getMainLooper()).postDelayed({
+            holder.skeleton.showOriginal()
+        }, 1000)
+
+        Picasso.get()
+            .load(productMainImages[position])
+            .placeholder(R.drawable.blank)
+            .into(holder.prodMainImage)
+
+        holder.prodName.text = productNames[position]
+        holder.realPrice.text = "₹${productRealPrices[position]}"
+        holder.discount.text = "${productDiscounts[position]}% ↓"
+        holder.discountedPrice.text = "₹${productDiscountedPrices[position]}"
+        holder.rating.rating = productRatings[position].toFloat()
+        holder.ratingCount.text = "(${productRatingCounts[position]})"
+
+        holder.selectedProductCard.setOnClickListener {
+            val intent = Intent(holder.itemView.context, ProductActivity::class.java)
+            intent.putExtra("productId", productIds[position])
+            holder.itemView.context.startActivity(intent)
+        }
     }
+
+    private fun updateData() {
+        CoroutineScope(Dispatchers.IO).launch {
+            try{
+                val result = db.collection("products").get().await()
+
+                for (document in result) {
+                    productIds += document.id
+                    Log.d("TAG111", "updateData: ${document.id}")
+                    productMainImages += document.getString("prodMainImage").toString()
+                    productNames += document.getString("productName").toString()
+                    Log.d("TAG111", "updateData: ${document.getString("productName").toString()}")
+                    productRealPrices += document.getLong("productPrice")?.toString()?: "0"
+                    productDiscounts += document.getLong("productDiscount")?.toString()?:"0"
+                    productDiscountedPrices += document.getLong("productDiscountedPrice")?.toString()?:"0"
+                    productRatings += document.getDouble("prodRating")?.toString()?:"0"
+                    productRatingCounts += document.getLong("prodRatingCount")?.toString()?:"0"
+                    productStock += document.getLong("productStock").toString()
+                }
+
+                withContext(Dispatchers.Main){
+                    notifyDataSetChanged()
+                }
+            }catch (e: Exception){
+                Log.d("TAG111", "updateData: ${e.message}")
+            }
+        }
+    }
+
+    fun updateData(query: String) {
+        val filteredProductIds = productIds.filter { productNameContainsQuery(it, query) }
+        val filteredProductMainImages = filteredProductIds.map { productMainImages[productIds.indexOf(it)] }
+        val filteredProductNames = filteredProductIds.map { productNames[productIds.indexOf(it)] }
+        val filteredProductRealPrices = filteredProductIds.map { productRealPrices[productIds.indexOf(it)] }
+        val filteredProductDiscounts = filteredProductIds.map { productDiscounts[productIds.indexOf(it)] }
+        val filteredProductDiscountedPrices = filteredProductIds.map { productDiscountedPrices[productIds.indexOf(it)] }
+        val filteredProductRatings = filteredProductIds.map { productRatings[productIds.indexOf(it)] }
+        val filteredProductRatingCounts = filteredProductIds.map { productRatingCounts[productIds.indexOf(it)] }
+        val filteredProductStock = filteredProductIds.map { productStock[productIds.indexOf(it)] }
+
+        productIds = filteredProductIds
+        productMainImages = filteredProductMainImages
+        productNames = filteredProductNames
+        productRealPrices = filteredProductRealPrices
+        productDiscounts = filteredProductDiscounts
+        productDiscountedPrices = filteredProductDiscountedPrices
+        productRatings = filteredProductRatings
+        productRatingCounts = filteredProductRatingCounts
+        productStock = filteredProductStock
+
+        notifyDataSetChanged()
+    }
+
+    private fun productNameContainsQuery(productId: String, query: String): Boolean {
+        val productName = productNames[productIds.indexOf(productId)]
+        return productName.contains(query, ignoreCase = true)
+    }
+
+    public fun updateData(sortBy : String, category : String, outOfStock : Boolean) {
+        CoroutineScope(Dispatchers.IO).launch {
+            try{
+                val result = db.collection("products").get().await()
+
+                for (document in result) {
+                    productIds += document.id
+                    Log.d("TAG111", "updateData: ${document.id}")
+                    productMainImages += document.getString("prodMainImage").toString()
+                    productNames += document.getString("productName").toString()
+                    Log.d("TAG111", "updateData: ${document.getString("productName").toString()}")
+                    productRealPrices += document.getLong("productPrice")?.toString()?: "0"
+                    productDiscounts += document.getLong("productDiscount")?.toString()?:"0"
+                    productDiscountedPrices += document.getLong("productDiscountedPrice")?.toString()?:"0"
+                    productRatings += document.getDouble("prodRating")?.toString()?:"0"
+                    productRatingCounts += document.getLong("prodRatingCount")?.toString()?:"0"
+                    productStock += document.getLong("productStock").toString()
+                }
+
+                val sortedProductIds = when(sortBy){
+                    "PriceLowToHigh" -> {
+                        productIds.sortedBy { productRealPrices[productIds.indexOf(it)].toInt() }
+                    }
+                    "PriceHighToLow" -> {
+                        productIds.sortedByDescending { productRealPrices[productIds.indexOf(it)].toInt() }
+                    }
+                    "RatingLowToHigh" -> {
+                        productIds.sortedBy { productRatings[productIds.indexOf(it)].toFloat() }
+                    }
+                    "RatingHighToLow" -> {
+                        productIds.sortedByDescending { productRatings[productIds.indexOf(it)].toFloat() }
+                    }
+                    "DiscountLowToHigh" -> {
+                        productIds.sortedBy { productDiscounts[productIds.indexOf(it)].toInt() }
+                    }
+                    "DiscountHighToLow" -> {
+                        productIds.sortedByDescending { productDiscounts[productIds.indexOf(it)].toInt() }
+                    }
+                    else -> {
+                        productIds
+                    }
+                }
+
+                productIds = sortedProductIds
+
+                if (category != "All Categories"){
+                    productIds = productIds.filter { category == db.collection("products").document(it).get().await().getString("productCategory").toString() }
+                }
+
+                if (outOfStock){
+                    productIds = productIds.filter { productStock[productIds.indexOf(it)].toInt() == 0 }
+                }
+
+                withContext(Dispatchers.Main){
+                    notifyDataSetChanged()
+                }
+
+            }
+            catch (e: Exception) {
+                Log.d("TAG111", "updateData: ${e.message}")
+            }
+        }
+    }
+
+    public fun updateData(sortBy : String, outOfStock : Boolean) {
+        CoroutineScope(Dispatchers.IO).launch {
+            try{
+                val result = db.collection("products").get().await()
+
+                for (document in result) {
+                    productIds += document.id
+                    Log.d("TAG111", "updateData: ${document.id}")
+                    productMainImages += document.getString("prodMainImage").toString()
+                    productNames += document.getString("productName").toString()
+                    Log.d("TAG111", "updateData: ${document.getString("productName").toString()}")
+                    productRealPrices += document.getLong("productPrice")?.toString()?: "0"
+                    productDiscounts += document.getLong("productDiscount")?.toString()?:"0"
+                    productDiscountedPrices += document.getLong("productDiscountedPrice")?.toString()?:"0"
+                    productRatings += document.getDouble("prodRating")?.toString()?:"0"
+                    productRatingCounts += document.getLong("prodRatingCount")?.toString()?:"0"
+                    productStock += document.getLong("productStock").toString()
+                }
+
+                val sortedProductIds = when(sortBy){
+                    "PriceLowToHigh" -> {
+                        productIds.sortedBy { productRealPrices[productIds.indexOf(it)].toInt() }
+                    }
+                    "PriceHighToLow" -> {
+                        productIds.sortedByDescending { productRealPrices[productIds.indexOf(it)].toInt() }
+                    }
+                    "RatingLowToHigh" -> {
+                        productIds.sortedBy { productRatings[productIds.indexOf(it)].toFloat() }
+                    }
+                    "RatingHighToLow" -> {
+                        productIds.sortedByDescending { productRatings[productIds.indexOf(it)].toFloat() }
+                    }
+                    "DiscountLowToHigh" -> {
+                        productIds.sortedBy { productDiscounts[productIds.indexOf(it)].toInt() }
+                    }
+                    "DiscountHighToLow" -> {
+                        productIds.sortedByDescending { productDiscounts[productIds.indexOf(it)].toInt() }
+                    }
+                    else -> {
+                        productIds
+                    }
+                }
+
+                productIds = sortedProductIds
+
+                if (outOfStock){
+                    productIds = productIds.filter { productStock[productIds.indexOf(it)].toInt() == 0 }
+                }
+
+                withContext(Dispatchers.Main){
+                    notifyDataSetChanged()
+                }
+
+            }
+            catch (e: Exception){
+                Log.d("TAG111", "updateData: ${e.message}")
+            }
+        }
+    }
+
 }
