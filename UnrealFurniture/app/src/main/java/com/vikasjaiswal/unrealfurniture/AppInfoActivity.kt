bd36d9b9243c9b39ae3db4560c2e851b9a4e1919
Vikas Jaiswal <vikasjaiswal.coder@gmail.com>
2024-03-14 21:50:04 +0530

    Small Modifications and Added UI for AppInfo Fragment

--- UnrealFurniture/app/src/main/java/com/vikasjaiswal/unrealfurniture/AppInfoActivity.kt
+++ UnrealFurniture/app/src/main/java/com/vikasjaiswal/unrealfurniture/AppInfoActivity.kt
@@ -1,11 +1,18 @@
 package com.vikasjaiswal.unrealfurniture
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class AppInfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.appinfo_activity)
+    }
+    
+    fun openSupport(view: View) {
+        val intent =  Intent(this@AppInfoActivity, SupportActivity::class.java)
+        startActivity(intent)
     }
 }
